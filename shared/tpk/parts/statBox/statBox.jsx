72d12fbe433425ff4a3f0575b22deb91810b800e
Scott Tolksdorf <scott.tolksdorf@gmail.com>
2016-09-04 17:33:14 -0400

    Statboxes upgrade

--- shared/tpk/parts/statBox/statBox.jsx
+++ shared/tpk/parts/statBox/statBox.jsx
@@ -3,67 +3,66 @@ var _ = require('lodash');
 var cx = require('classnames');
 
 var Box = require('../box/box.jsx');
-var utils = require('../utils');
 
 var StatBox = React.createClass({
 	getDefaultProps: function() {
 		return {
-			name : 'statBox',
-			defaultData : {
+			base_name : 'statBox',
+			data : {
 				value : '',
 				mod : '',
 				alt : '',
 			},
 
+			label : '',
+			title : '',
 			mod : false,
 			alt : false,
-
-			width : false,
-
-			border : true,
 		};
 	},
 
-	id : utils.id,
-	data : utils.data,
-	updateData : utils.updateData,
-
 	handleValueChange : function(e){
-		this.updateData({
+		this.props.onChange(_.assign({}, this.props.data, {
 			value : e.target.value
-		});
+		}));
 	},
 
 	handleModChange : function(e){
-		this.updateData({
+		this.props.onChange(_.assign({}, this.props.data, {
 			mod : e.target.value
-		});
+		}));
 	},
 
 	handleAltChange : function(e){
-		this.updateData({
+		this.props.onChange(_.assign({}, this.props.data, {
 			alt : e.target.value
-		})
+		}));
 	},
 
 	renderAlt : function(){
 		if(!this.props.alt) return null;
 		return <div className='alt'>
-			<input type='text' onChange={this.handleAltChange} value={this.data().alt} />
+			<input type='text' onChange={this.handleAltChange} value={this.props.data.alt} />
 		</div>
 	},
 
 	renderMod : function(){
 		if(!this.props.mod) return null;
 		return <div className='mod'>
-			<input type='text' onChange={this.handleModChange} value={this.data().mod} />
+			<input type='text' onChange={this.handleModChange} value={this.props.data.mod} />
 		</div>
 	},
 
 	render : function(){
-		return <Box className={cx('statBox', {mod : this.props.mod, alt : this.props.alt})} {...this.props}>
+		return <Box
+				className={cx('statBox', {mod : this.props.mod, alt : this.props.alt})}
+				border={true}
+				label={this.props.label}
+				title={this.props.title}
+				style={this.props.style}
+				>
 			{this.renderMod()}
-			<input className='value' type='text' value={this.data().value} onChange={this.handleValueChange} />
+			<input className='value' type='text' value={this.props.data.value} onChange={this.handleValueChange} />
 			{this.renderAlt()}
 		</Box>
 	}
